fb2e08b2dfe1f7a681d9fc47ef1c9e532935b77e
mohammed <mohamedelboustani470@gmail.com>
2026-09-14 14:25:34 +0100

    calcul the area

---
+++ cpp_learn/area.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    int b;
+    cout << "enter the value of a and b \n";
+    cin>>a;
+    cin>>b;
+    int area=a*b;
+    cout<<"area of rectanglle is \n"<<area<<endl;
+    return 0;
+}
